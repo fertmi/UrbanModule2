ac112fc42111b4e749209696d82c8e07bf38f934
fertmi <fertmi@mail.ru>
2024-11-22 16:45:40 +0300

    Zeros are nothing, negation is unacceptable! ver3

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index_list = 0
 length_list = len(my_list)
-while index_list < length_list:
+while index_list < length_list and my_list[index_list]>=0:
     if my_list[index_list] > 0:
         print('Положительное число из списка:',my_list[index_list])
-    elif my_list[index_list] < 0:
-        break
-    index_list = index_list + 1
+    index_list += 1
