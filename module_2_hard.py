35795fcc403e2ed347d78c8c2dcccc987513531b
fertmi <fertmi@mail.ru>
2024-11-25 11:15:52 +0300

    The cipher is too old variant 2 with while

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,12 @@
 n = int(input('Введите число от 3 до 20: '))
 variable = 20
+first_pair = 1
 result = []
-for i in range(1,21):
-    if i>=variable:
-        break
-    for j in range(i+1,n):
-        if n % (i+j) == 0:
-            result.append(i)
-            result.append(j)
-            variable = j
+while first_pair<=variable:                    # цикл для первого числа пары
+    for second_pair in range(first_pair+1,n):  # цикл для второго числа пары
+        if n % (first_pair+second_pair) == 0:  # если заданное число делится без остатка на сумму пары
+            result.append(first_pair)          # запись в список первого числа пары
+            result.append(second_pair)         # запись в список второго числа пары
+            variable = second_pair
+    first_pair += 1
 print('Пароль:',*result, sep='')
