1b552f96335eb9dc4a95be54f9bacf4ba0759c16
fertmi <fertmi@mail.ru>
2024-11-25 11:57:39 +0300

    The cipher is too old var 3 with while and fuction

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,20 @@
-n = int(input('Введите число от 3 до 20: '))
-variable = 20
-first_pair = 1
-result = []
-while first_pair<=variable:                    # цикл для первого числа пары
-    for second_pair in range(first_pair+1,n):  # цикл для второго числа пары
-        if n % (first_pair+second_pair) == 0:  # если заданное число делится без остатка на сумму пары
-            result.append(first_pair)          # запись в список первого числа пары
-            result.append(second_pair)         # запись в список второго числа пары
-            variable = second_pair
-    first_pair += 1
-print('Пароль:',*result, sep='')
+def cipher():
+    variable = 20
+    first_pair = 1
+    result = []
+    while first_pair<=variable:                    # цикл для первого числа пары c условием
+        for second_pair in range(first_pair+1,n):  # цикл для второго числа пары
+            if n % (first_pair+second_pair) == 0:  # если заданное число делится без остатка на сумму пары
+                result.append(first_pair)          # запись в список первого числа пары
+                result.append(second_pair)         # запись в список второго числа пары
+                variable = second_pair
+        first_pair += 1
+    return result
+reply = 0
+while reply == 0:                                   # цикл для повторения ввода числа первой пары
+    n = int(input('Введите число от 3 до 20, для выхода 0: '))
+    if n == 0:
+        print('До свидания')
+        break
+    result = cipher()
+    print('Пароль:', *result, sep='')
